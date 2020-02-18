34537b23cc52623cfe72e89102c2003a75aca9cf
Claudinei Bido <bido.c@live.com>
2020-02-18 13:46:22 +0100

    changes

--- index.js
+++ index.js
@@ -1,15 +1,6 @@
-/**
- * ls
- * Create a function that takes a string and returns a new string with all vowels removed.
- *
- * Examples:
- *
- * removeVowels("I have never seen a thin person drinking Diet Coke.") ➞ " hv nvr sn  thn prsn drnkng Dt Ck."
- * removeVowels("We're gonna build a wall!") ➞ "W'r gnn bld  wll!"
- * removeVowels("Happy Thanksgiving to all--even the haters and losers!") ➞ "Hppy Thnksgvng t ll--vn th htrs nd lsrs!"
- * Notes: "y" is not considered a vowel.
- *
- */
-
 const removeVowels = (str) => str.replace(/[aeiou]/gi, "");
 console.log(removeVowels("I have never seen a thin person drinking Diet Coke"));
+console.log(removeVowels("We're gonna build a wall!"));
+console.log(
+  removeVowels("Happy Thanksgiving to all--even the haters and losers!")
+);
